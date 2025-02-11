a20d60c76d9a77abd5d6ee4c346f13977d91c4a2
DavidFrontendDev <daviddevbackend@gmail.com>
2025-02-11 17:39:55 +0100

    App jsx y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import "./App.css";
-
+import { desafios } from "./services/data";
 function App() {
-  return <></>;
+  const [desafio, setDesafio] = useState("");
+
+  function desafioRandom() {
+    const desafioRandom = desafios[Math.floor(Math.random() * desafios.length)];
+    setDesafio(desafioRandom);
+  }
+
+  return (
+    <>
+      <header>
+        <div className="gd-generar">
+          <h2>Generador de Desafios Creativos 😂</h2>
+          <button className="gd-boton-generar" onClick={() => desafioRandom()}>
+            Generar
+          </button>
+          <div className="gd-crear">
+            <input placeholder="Crea tu propio desafio..." />
+            <button className="gd-boton-generar">Crear</button>
+          </div>
+          <button className="gd-fondo-oscuro">🌙🌞</button>
+        </div>
+      </header>
+      <main></main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Desafio.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// Like Button TODO
+function Desafio() {
+  return <div></div>;
+}
+
+export default Desafio;
